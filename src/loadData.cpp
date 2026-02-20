282dea4fa93573abf6e78c2f30a5ee76fd038154
Abuchi Osuala <osualajames@gmail.com>
2026-02-20 14:19:04 -0600

    Breaking up ECG data loading into seperate functions

--- src/loadData.cpp
+++ src/loadData.cpp
@@ -4,23 +4,20 @@
 #include <vector>
 #include <sstream>
 
-int main() {
+std::pair<std::vector<double>, std::vector<double>> storeData(std::ifstream& file) {
     std::vector<double> timeData;
     std::vector<double> signalData;
-
-    std::ifstream inputFile("../ecgData/data.csv");
     std::string line;
 
-    if(!inputFile.is_open()) {
-        std::cerr << "Error opening file\n";
-        return 1;
+    if(!file.is_open()) {
+        std::cerr << "File does not exist\n";
+        return {{}, {}};
     }
 
-    std::cout << "Reading in file\n";
-    while(std::getline(inputFile, line)) {
+    std::cout<<"Loading data from file\n";
+    while(std::getline(file, line)) {
         std::stringstream ss(line);
         std::string timeStr, signalStr;
-
         if (std::getline(ss, timeStr, ',') && std::getline(ss, signalStr)) {
             double time = std::stod(timeStr);
             double signal = std::stod(signalStr);
@@ -31,6 +28,13 @@ int main() {
     if (timeData.size() == signalData.size()) {
         std::cout << "Data Loaded\n";
     }
+    return {timeData, signalData};
+}
+
+int main() {
+    std::ifstream inputFile("../ecgData/data.csv");
+    auto [timeData, signalData] = storeData(inputFile);
+    std::cout << timeData.size() << signalData.size();
     inputFile.close();
 
 }
